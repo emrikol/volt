e6fd22e0f9be9fc74e07c53ed701552cf9354f59
emrikol <derrick.tennant@automattic.com>
2017-01-06 01:19:18 +0000

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		sass: {
+			all: {
+				options: {
+					unixNewlines: true
+				},
+				files: {
+					'style.css': 'scss/style.scss',
+					'css/menu.css': 'scss/menu.scss'
+				}
+			}
+		},
+
+		watch: {
+			styles: {
+				files: [ 'scss/**/*.scss' ],
+				tasks: [ 'sass' ],
+				options: {
+					debounceDelay: 500
+				}
+			}
+		},
+
+		clean: {
+			main: ['release/<%= pkg.version %>']
+		},
+
+		copy: {
+			main: {
+				src:  [
+					'**',
+					'!**/.*',
+					'!**/style.css.map',
+					'!**/readme.md',
+					'!node_modules/**',
+					'!release/**',
+					'!scss/**',
+					'!fonts/**',
+					'!images/src/**',
+					'!composer.json',
+					'!composer.lock',
+					'!Gruntfile.js',
+					'!package.json',
+				],
+				dest: 'release/<%= pkg.version %>/'
+			}
+		},
+
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: './release/<%= pkg.name %>.<%= pkg.version %>.zip'
+				},
+				expand: true,
+				cwd: 'release/<%= pkg.version %>/',
+				src: ['**/*'],
+				dest: '<%= pkg.name %>/'
+			}
+		},
+
+	} );
+
+	require( 'load-grunt-tasks' )(grunt);
+
+	grunt.registerTask( 'default', ['concat', 'uglify', 'sass', 'cssmin' ] );
+	grunt.registerTask( 'css', ['sass', 'autoprefixer', 'cssmin'] );
+	grunt.registerTask( 'js', ['concat', 'uglify'] );
+	grunt.registerTask( 'build', ['default', 'clean', 'copy', 'compress'] );
+
+	grunt.util.linefeed = '\n';
+};
