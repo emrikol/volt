bccab2562e40b7726e89bcd3643ddc36a66fe89d
emrikol <derrick.tennant@automattic.com>
2017-01-26 18:54:59 +0000

    Don't want to hide the date

--- template-parts/post-byline.php
+++ template-parts/post-byline.php
@@ -1,34 +1,19 @@
-<?php
-
-$author_display = get_theme_mod( 'display_post_author' );
-$date_display   = get_theme_mod( 'display_post_date' );
-
-if ( 'hide' === $author_display && 'hide' === $date_display ) {
-	return;
-}
-
-?>
-
 <div class="post-byline">
-	<?php if ( 'hide' !== $author_display ) : ?>
-		<span class="post-author">
-			<span><?php esc_html_e( 'By', 'volt' ); ?></span>
-			<?php the_author(); ?>
-		</span>
-	<?php endif; ?>
-	<?php if ( 'hide' !== $date_display ) : ?>
-		<span class="post-date">
-			<span>
-				<?php
-				if ( 'hide' !== $author_display ) {
-					esc_html_e( 'on', 'volt' );
-				}
-				?>
-			</span>
+	<span class="post-author">
+		<span><?php esc_html_e( 'By', 'volt' ); ?></span>
+		<?php the_author(); ?>
+	</span>
+	<span class="post-date">
+		<span>
 			<?php
-			$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
-			echo esc_html( $date );
+			if ( 'hide' !== $author_display ) {
+				esc_html_e( 'on', 'volt' );
+			}
 			?>
 		</span>
-	<?php endif; ?>
+		<?php
+		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
+		echo esc_html( $date );
+		?>
+	</span>
 </div>
