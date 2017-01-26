be72b9d7cdb04d85649bc71c5feb6af6de60eee9
emrikol <derrick.tennant@automattic.com>
2017-01-26 20:32:25 +0000

    post-date now links to permalink

--- template-parts/post-byline.php
+++ template-parts/post-byline.php
@@ -4,11 +4,9 @@
 		<?php the_author(); ?>
 	</span>
 	<span class="post-date">
-		<span><?php esc_html_e( 'on', 'volt' ); ?>
-		</span>
-		<?php
-		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
-		echo esc_html( $date );
-		?>
+		<span><?php esc_html_e( 'on', 'volt' ); ?></span>
+		<a class="permalink" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+			<?php echo esc_html( date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) ) ); ?>
+		</a>
 	</span>
 </div>
