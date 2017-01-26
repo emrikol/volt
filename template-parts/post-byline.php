09ab90d95669a516bc96e90f4f16ad229aadd4bf
emrikol <derrick.tennant@automattic.com>
2017-01-26 18:55:27 +0000

    Don't want to hide the date

--- template-parts/post-byline.php
+++ template-parts/post-byline.php
@@ -4,12 +4,7 @@
 		<?php the_author(); ?>
 	</span>
 	<span class="post-date">
-		<span>
-			<?php
-			if ( 'hide' !== $author_display ) {
-				esc_html_e( 'on', 'volt' );
-			}
-			?>
+		<span><?php esc_html_e( 'on', 'volt' ); ?>
 		</span>
 		<?php
 		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
